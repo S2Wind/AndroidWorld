c8645d195450e996acee40a45fea64130c0e942f
SONPD <phandacson213@gmail.com>
2023-10-19 08:03:14 +0700

    FirstButtonActivity

--- FirstJavaApplication/app/src/main/java/com/example/firstjavaapplication/MainActivity.java
+++ FirstJavaApplication/app/src/main/java/com/example/firstjavaapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.firstjavaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void handleText(View v)
+    {
+        EditText t = findViewById(R.id.source);
+        String input = t.getText().toString();
+//        ((TextView)findViewById(R.id.output).setTe)
+        Toast.makeText(this,input,Toast.LENGTH_LONG).show();
     }
 }
